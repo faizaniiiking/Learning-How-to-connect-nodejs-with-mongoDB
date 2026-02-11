e57e5295e7b177c4ee572eee35448a73b5ce92ac
Faizan Kureshi <faizanikkhan@gmail.com>
2026-02-11 20:47:07 +0530

    2ns method to ad mongoddb with nodejs

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
-const { MongoClient } = require("mongodb");
+// const { MongoClient } = require("mongodb");
 
-const url = "mongodb://127.0.0.1:27017";
-const client = new MongoClient(url);
-async function connectDB() {
-  try {
-    await client.connect();
-    console.log("✅ Connected to MongoDB");
+// const url = "mongodb://127.0.0.1:27017";
+// const client = new MongoClient(url);
+// async function connectDB() {
+//   try {
+//     await client.connect();
+//     console.log("✅ Connected to MongoDB");
 
-    const db = client.db("bollywood");
-    const collection = db.collection("actor");
+//     const db = client.db("sport");
+//     const collection = db.collection("cricket");
 
-    await collection.insertOne({
-      name: "salman khan",
-      age: 30
-    });
+//     await collection.insertOne({
+//       name: "virat",
+//       age: 30
+//     });
 
-    console.log("✅ Data Inserted");
+//     console.log("✅ Data Inserted");
 
-  } catch (err) {
-    console.log("❌ Error:", err);
-  }
-}
+//   } catch (err) {
+//     console.log("❌ Error:", err);
+//   }
+// }
 
-connectDB();
+// connectDB();
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/salmankhan")
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch(err => console.log("❌ Connection Error", err));
+
+  const studentSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+const Student = mongoose.model("Actor", studentSchema);
+
+Student.create({
+  name: "Faizan",
+  age: 20
+});
+
